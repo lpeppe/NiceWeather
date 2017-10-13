f6d12380541c70aeef3921814bf86b9dcad847fb
Luca Peppe <luca@Lucas-MacBook-Pro.local>
2017-10-13 10:38:52 +0200

    Added map and input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+ import { AutoCompleteModule } from 'ionic2-auto-complete';
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AutoCompleteModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +35,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GoogleMaps
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +16,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: GoogleMap;
+  mapElement: HTMLElement;
 
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps) {
   }
 
+  ionViewDidLoad() {
+    this.loadMap();
+   }
+
+   loadMap() {
+    this.mapElement = document.getElementById('map');
+    
+        let mapOptions: GoogleMapOptions = {
+          camera: {
+            target: {
+              lat: 43.0741904,
+              lng: -89.3809802
+            },
+            zoom: 18,
+            tilt: 30
+          }
+        };
+    
+        this.map = this.googleMaps.create(this.mapElement, mapOptions);
+   }
 }
