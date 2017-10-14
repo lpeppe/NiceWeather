1af5028935e6c98a026a5865b3484e10886e3d62
Luca Peppe <luca@Lucas-MacBook-Pro.local>
2017-10-14 12:48:11 +0200

    Added geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,7 @@ import {
   Marker
  } from '@ionic-native/google-maps';
 import { AutoCompleteModule } from 'ionic2-auto-complete';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -20,28 +21,51 @@ export class HomePage {
   map: GoogleMap;
   mapElement: HTMLElement;
 
-  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps) {
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps, public platform: Platform) {
   }
 
-  ionViewDidLoad() {
-    this.loadMap();
-   }
+  // ionViewDidLoad() {
+  //   this.loadMap();
+  //  }
+
+   ngAfterViewInit() {
+    this.platform.ready().then(() => {
+      this.loadMap();
+    });
+  }
 
    loadMap() {
     this.mapElement = document.getElementById('map');
     
         let mapOptions: GoogleMapOptions = {
-          camera: {
-            target: {
-              lat: 43.0741904,
-              lng: -89.3809802
-            },
-            zoom: 18,
-            tilt: 30
-          }
+          // camera: {
+          //   target: {
+          //     lat: 43.0741904,
+          //     lng: -89.3809802
+          //   },
+          //   zoom: 18,
+          //   tilt: 30
+          // }
         };
     
         // this.map = this.googleMaps.create(this.mapElement, mapOptions);
         this.map = new GoogleMap(this.mapElement, mapOptions);
+        this.map.one(GoogleMapsEvent.MAP_READY)
+        .then(_ => {
+          this.map.getMyLocation()
+          .then(location => {
+            let lat = location.latLng.lat;
+            let lng = location.latLng.lng;
+            let position: CameraPosition = {
+              target: {
+                lat: lat,
+                lng: lng
+              },
+              zoom: 18,
+              tilt: 30
+            };
+            this.map.moveCamera(position);
+          })
+        })
    }
 }
