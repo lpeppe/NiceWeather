7e871976b185dc17f9b3636ffdffa83abaaf382d
luca <lcpp1994@gmail.com>
2018-01-17 12:48:32 +0100

    minor bugfix

--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-@IonicPage()
+// @IonicPage()
 @Component({
   selector: 'page-popover',
   templateUrl: 'popover.html',
