7af5a089e25f73307932767a053563d8eecc0e29
mariaelena <m.cammarano25@studenti.unisa.it>
2018-02-03 11:48:47 +0100

    Style ski list

--- src/components/ski-details/ski-details.ts
+++ src/components/ski-details/ski-details.ts
@@ -16,33 +16,37 @@ export class SkiDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.pisteDetails.push({
-      name: 'height',
-      value: this.selectedPiste.height
-    });
-    this.pisteDetails.push({
-      name: 'numPiste',
-      value: this.selectedPiste.numPiste
-    });
-    this.pisteDetails.push({
-      name: 'pisteLength',
-      value: this.selectedPiste.pisteLength
-    });
+    // this.pisteDetails.push({
+    //   name: 'height',
+    //   value: this.selectedPiste.height
+    // });
+    // this.pisteDetails.push({
+    //   name: 'numPiste',
+    //   value: this.selectedPiste.numPiste
+    // });
+    // this.pisteDetails.push({
+    //   name: 'pisteLength',
+    //   value: this.selectedPiste.pisteLength
+    // });
     this.pisteDetails.push({
       name: 'blackPiste',
-      value: this.selectedPiste.blackPiste
+      value: this.selectedPiste.blackPiste,
+      color: 'black'
     });
     this.pisteDetails.push({
       name: 'bluePiste',
-      value: this.selectedPiste.bluePiste
+      value: this.selectedPiste.bluePiste,
+      color: 'primary'
     });
     this.pisteDetails.push({
       name: 'redPiste',
-      value: this.selectedPiste.redPiste
+      value: this.selectedPiste.redPiste,
+      color: 'red'
     });
     this.pisteDetails.push({
       name: 'greenPiste',
-      value: this.selectedPiste.greenPiste
+      value: this.selectedPiste.greenPiste,
+      color: 'green'
     });
   }
 }
