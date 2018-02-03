e1ddd2fc003a3c30df1a5d2ee0092e1090283ff2
luca <lcpp1994@gmail.com>
2018-02-03 12:32:47 +0100

    fixed ski-details bug

--- src/components/ski-details/ski-details.ts
+++ src/components/ski-details/ski-details.ts
@@ -1,52 +1,68 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { SkiDetails } from './../../models/interfaces';
 import { StatusProvider } from './../../providers/status/status';
+import { DataProvider } from '../../providers/data/data';
+import { Subscription } from 'rxjs/Subscription';
+import { SelectedActivity } from './../../models/enums';
 
 @Component({
   selector: 'ski-details',
   templateUrl: 'ski-details.html'
 })
-export class SkiDetailsComponent implements OnInit {
+export class SkiDetailsComponent implements OnInit, OnDestroy {
 
-  @Input() selectedPiste: SkiDetails;
   pisteDetails: any[];
+  subscriptions: Subscription[];
+  pisteName: string;
+  numPiste: number;
 
-  constructor(public statusProvider: StatusProvider) {
+  constructor(public statusProvider: StatusProvider, public dataProvider: DataProvider) {
     this.pisteDetails = [];
+    this.subscriptions = [];
   }
 
   ngOnInit() {
-    // this.pisteDetails.push({
-    //   name: 'height',
-    //   value: this.selectedPiste.height
-    // });
-    // this.pisteDetails.push({
-    //   name: 'numPiste',
-    //   value: this.selectedPiste.numPiste
-    // });
-    // this.pisteDetails.push({
-    //   name: 'pisteLength',
-    //   value: this.selectedPiste.pisteLength
-    // });
+    this.subscriptions.push(
+      this.statusProvider.placeSelected
+        .subscribe(async id => {
+          if (id) {
+            this.pisteDetails.splice(0, this.pisteDetails.length);
+            let details: SkiDetails = await this.dataProvider
+              .getActivityDetails(<any>SelectedActivity[SelectedActivity.ski], id)
+            this.pisteName = details.name;
+            this.numPiste = details.numPiste;
+            this.addPisteDetails(details);
+          }
+        })
+    )
+  }
+
+  ngOnDestroy() {
+    for (let subscription of this.subscriptions)
+      subscription.unsubscribe();
+  }
+
+  addPisteDetails(details: SkiDetails) {
     this.pisteDetails.push({
       name: 'blackPiste',
-      value: this.selectedPiste.blackPiste,
+      value: details.blackPiste,
       color: 'black'
     });
     this.pisteDetails.push({
       name: 'bluePiste',
-      value: this.selectedPiste.bluePiste,
+      value: details.bluePiste,
       color: 'primary'
     });
     this.pisteDetails.push({
       name: 'redPiste',
-      value: this.selectedPiste.redPiste,
+      value: details.redPiste,
       color: 'red'
     });
     this.pisteDetails.push({
       name: 'greenPiste',
-      value: this.selectedPiste.greenPiste,
+      value: details.greenPiste,
       color: 'green'
     });
   }
+
 }
